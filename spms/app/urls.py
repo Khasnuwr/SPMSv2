5a5c421d7a8cc55cb0538e1b4a8bbee06f16ac53
Arif <smarifmahmud9@gmail.com>
2023-03-27 05:26:46 +0600

    starting

---
+++ spms/app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+]
---
+++ spms/app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+
+# Logout User Function
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+# Login Function and Page
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            user = authenticate(
+                request, username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.add_message(request, messages.INFO,
+                                     'Wrong username or password')
+                return redirect('login')
+        return render(request, 'login/login.html', {})
+
+# Displaying Home Page Function
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'home/home.html', {})
+    else:
+        return redirect('login')
