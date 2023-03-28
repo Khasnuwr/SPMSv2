771bc0ffdd76c930986cd456ad5cb54167d5c1f9
Arif <smarifmahmud9@gmail.com>
2023-03-29 05:04:34 +0600

    abstract-user

--- spms/app/admin.py
+++ spms/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+# Assigning Field list in 'fields' variable
+fields = list(UserAdmin.fieldsets)
+# Serializing these fields in the fields[1] index of list fields
+fields[1] = ('Personal Info', { 'fields': ('first_name',  'last_name', 'email', 'phone', 'program', 'department', 'role')})
+# Storing fields list as tuple in UserAdmin fieldsets
+UserAdmin.fieldsets = tuple(fields)
+# Registering User_T in the AdminPanel
+admin.site.register(User_T, UserAdmin)
+admin.site.register(Program_T)
+admin.site.register(Department_T)
+admin.site.register(Course_T)
+admin.site.register(School_T)
--- spms/app/models.py
+++ spms/app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-
 # School Database Table
 class School_T(models.Model):
     schoolID = models.CharField(max_length=10, primary_key=True,null=False, blank=False)
@@ -11,7 +10,7 @@ class School_T(models.Model):
 # Department Database Table
 class Department_T(models.Model):
     departmentID = models.CharField(max_length=10, primary_key=True,null=False, blank=False)
-    departmentName = models.CharField(max_length=255, ,null=False, blank=False)
+    departmentName = models.CharField(max_length=255, null=False, blank=False)
     schoolID = models.ForeignKey(School_T, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -34,3 +33,18 @@ class Course_T(models.Model):
 
     def __str__(self):
         return str(self.courseID)
+# Section Table
+class Section_T(models.Model):
+    pass
+# Custom User Table
+class User_T(AbstractUser):
+    ROLES_CHOICES=(
+        ('Admin', 'Admin'),
+        ('Faculty', 'Faculty'),
+        ('Student', 'Student'),
+    )
+    role = models.CharField(max_length=30, choices=ROLES_CHOICES)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=30, null=True, blank=True)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE, null=True, blank=True)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, null=True, blank=True)
