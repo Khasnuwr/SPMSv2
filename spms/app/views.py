b0d6bd8c64aa9a224656dfdabd14f24e5e9dc509
Arif <smarifmahmud9@gmail.com>
2023-03-29 21:13:10 +0600

    fixing something

--- spms/app/views.py
+++ spms/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
+from django.contrib import messages
+
 
 # Logout User Function
 def logout_user(request):
