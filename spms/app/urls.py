3d3c93d4f4b23ade1c7888e3f06504786853bb49
Md Tuhin Al Jobayer <58110053+Eagl3Eyes@users.noreply.github.com>
2024-09-01 10:23:17 +0600

    update

--- spms/app/urls.py
+++ spms/app/urls.py
@@ -3,16 +3,20 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # Home
     path('', home, name='home'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+
     # Admin Path
     path('notice-page/', admin_page, name='notice-page'),
+
     # Faculty Path
     path('grade-input/', gradeInputForm, name='grade-input'),
     path('grade-input-csv/', gradeInputFromCSV, name='grade-input-csv'),
     path('generate-obe-format/', generate_obe_format, name='generate-obe-format'),
     path('generate-obe-csv/', generate_obe_csv, name='generate-obe-csv'),
+    
     # Student Download Transcript
     path('transcript/', genTranscript, name='transcript'),
 ]
